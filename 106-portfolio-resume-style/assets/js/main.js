6dd558f52ac9ed69b6a09abb5bf9154d5194f622
Naveen Manjappa <NManjappa@manh.com>
2025-03-24 13:38:28 +0000

    105-portfolio-resume Implement scroll to top

--- 106-portfolio-resume-style/assets/js/main.js
+++ 106-portfolio-resume-style/assets/js/main.js
@@ -43,3 +43,17 @@ function scrollActive() {
 }
 
 window.addEventListener('scroll',scrollActive);
+
+// Show Scroll Top //
+
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  if(this.scrollY >= 560){
+    scrollTop.classList.add('show-scroll');
+  }
+  else {
+    scrollTop.classList.remove('show-scroll');
+  }  
+}
+
+window.addEventListener('scroll',scrollTop);
