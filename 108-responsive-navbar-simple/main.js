d03e961c6c11d47b59860b87e5666b07d044ed85
Naveen Manjappa <NManjappa@manh.com>
2025-03-30 07:27:05 +0100

    108-responsive-navbar Implemented open and close navbar

--- 108-responsive-navbar-simple/main.js
+++ 108-responsive-navbar-simple/main.js
@@ -1,15 +1,45 @@
 const body = document.querySelector('body'),
 nav = document.querySelector('nav'),
 modeToggle = document.querySelector('.sidebar__theme'),
-searchToggle = document.querySelector('.sidebar__search__toggle');
+searchToggle = document.querySelector('.sidebar__search__toggle'),
+sidebarClose = document.querySelector('.sidebarClose'),sidebarOpen = document.querySelector('.sidebarOpen');
 
+let getMode = localStorage.getItem('mode');
+if(getMode && getMode === 'dark-mode'){
+  body.classList.add('dark');
+}
 //Dark or light theme
 modeToggle.addEventListener('click',() => {
   modeToggle.classList.toggle('active');
   body.classList.toggle('dark');
+
+  if(!body.classList.contains('dark')){
+    localStorage.setItem('mode','light-mode');
+  } else {
+    localStorage.setItem('mode','dark-mode');
+  }
 });
 
 //toggle search box
 searchToggle.addEventListener('click',()=> {
   searchToggle.classList.toggle('active');
 });
+
+//Toggle sidebar
+sidebarOpen.addEventListener('click',() => {
+  nav.classList.add('active');
+});
+
+sidebarClose.addEventListener('click',() => {
+  nav.classList.remove('active');
+});
+
+body.addEventListener('click',e => {
+  let clickedEl = e.target;
+  if(!clickedEl.classList.contains("sidebarOpen") && !clickedEl.classList.contains("nav__menu") ){
+    nav.classList.remove('active');
+  }
+});
+
+//Reference
+//https://youtu.be/TWiy3dGSmgk?si=S7IRE6NdndCmtm3n
